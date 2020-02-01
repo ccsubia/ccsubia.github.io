a1c970244d35f0754f1ec83b92202218bd1021e2
e <2656623598@qq.com>
2020-02-01 14:34:25 +0800

    Site updated: 2020-02-01 14:34:25

--- js/nowday - 副本.js
+++ js/nowday - 副本.js
@@ -14,45 +14,47 @@ Date.prototype.Format = function (fmt) {
     return fmt;
 }
 
-var myDate = new Date();
-// date = myDate.toLocaleDateString();
-date = myDate.Format("yyyy-MM-dd")
-//alert(date)
+var showDate = new Date(); // 定义显示日期时间戳
+var trueDate = new Date()  // 定义真实日期
+date = showDate.Format("yyyy-MM-dd")
 var update = document.getElementById("date");
 update.innerHTML = date;
-document.getElementById("picshow").src = 'https://bing.lylares.com/download/hd/' + date;
+document.getElementById("picshow").src = 'https://api.berryapi.net/?service=App.Bing.Images&day=-0'; // 初始化今日图片
 
-var i = 0
-
-
-function daydown(){
+function day_down(){ // 往前推一天
     // var dateTime=new Date();
-    dateTime=myDate.setDate(myDate.getDate()-1);
+    dateTime=showDate.setDate(showDate.getDate()-1);
     dateTime=new Date(dateTime);
-    myDate = dateTime
+    showDate = dateTime
     // return dateTime.toLocaleDateString()
-    return myDate
+    return showDate
 }
-function dayup(){
-    dateTime=myDate.setDate(myDate.getDate()+1);
+function day_up(){ // 往后推一天
+    dateTime=showDate.setDate(showDate.getDate()+1);
     dateTime=new Date(dateTime);
-    myDate = dateTime
+    showDate = dateTime
     // return dateTime.toLocaleDateString()
-    return myDate
+    return showDate
 }
-function beforeday() {
-    var newday = daydown().Format("yyyy-MM-dd")
+function beforeday() {  
+    var newday = day_down().Format("yyyy-MM-dd")
     document.getElementById("picshow").src = 'https://bing.lylares.com/download/hd/' + newday;
     update.innerHTML = newday
 }
 function nextday() {
-    var newdayc = dayup()
-    var trueday = new Date()
-    if (newdayc > trueday){
-        daydown()
+    var newdayc = day_up()
+    var update = document.getElementById("date");
+    if (newdayc > trueday){ // 请求图片日期未存在
+        day_down()
         alert('未发布')
+    }/* 
+    else if(newdayc == showDate){
+        // alert(showDate)
+        var newday = newdayc.Format("yyyy-MM-dd")
+        document.getElementById("picshow").src = 'https://api.berryapi.net/?service=App.Bing.Images&day=-0';
+        update.innerHTML = newday
     }
-    else{
+     */else{
         var newday = newdayc.Format("yyyy-MM-dd")
         document.getElementById("picshow").src = 'https://bing.lylares.com/download/hd/' + newday;
         update.innerHTML = newday
