b76c4e7c35c5cbd604f44570b0cddaae9e0fe76b
e <2656623598@qq.com>
2020-02-02 16:35:47 +0800

    Site updated: 2020-02-02 16:35:46

--- js/ayer.js
+++ js/ayer.js
@@ -252,7 +252,7 @@
             }
         }
     }
-    window.$crisp=[];window.CRISP_WEBSITE_ID="4f617ffa-2484-41ba-83d0-6703a91c362b";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+    
 })(jQuery);
 
 
